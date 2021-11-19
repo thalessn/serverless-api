a7d34849143dc3fbcc63734e89c93825d7130d69
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-11-18 23:26:38 -0300

    test: ensure CreateEmployeeController return 400 if property cargo not found

--- src/controllers/CreateEmployee/CreateEmployeeController.spec.ts
+++ src/controllers/CreateEmployee/CreateEmployeeController.spec.ts
@@ -2,7 +2,7 @@ import { CreateEmployeeController } from './CreateEmployeeController'
 import { MissingParamError } from '../errors/missing-param-error'
 
 describe('CreateEmployeeController', () => {
-  it('Should return error 400 if property name not found', () => {
+  it('Should return error 400 if property nome not found', () => {
     const sut = new CreateEmployeeController()
     const httpRequest = {
       body: {
@@ -12,19 +12,32 @@ describe('CreateEmployeeController', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('nome'))
   })
 
   it('Should return error 400 if property idade not found', () => {
     const sut = new CreateEmployeeController()
     const httpRequest = {
       body: {
-        name: 'John',
+        nome: 'John',
         cargo: 'chefia',
       },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('idade'))
+  })
+
+  it('Should return error 400 if property cargo not found', () => {
+    const sut = new CreateEmployeeController()
+    const httpRequest = {
+      body: {
+        nome: 'John',
+        idade: '30',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('cargo'))
   })
 })
