53efef5d56e776440e01932d4d016b3700f60001
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-11-18 23:12:46 -0300

    refactor: create the method MissingParamError

--- src/controllers/CreateEmployee/CreateEmployeeController.spec.ts
+++ src/controllers/CreateEmployee/CreateEmployeeController.spec.ts
@@ -1,4 +1,6 @@
 import { CreateEmployeeController } from './CreateEmployeeController'
+import { MissingParamError } from '../errors/missing-param-error'
+
 describe('CreateEmployeeController', () => {
   it('Should return error 400 if property name not found', () => {
     const sut = new CreateEmployeeController()
@@ -10,6 +12,19 @@ describe('CreateEmployeeController', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
+
+  it('Should return error 400 if property idade not found', () => {
+    const sut = new CreateEmployeeController()
+    const httpRequest = {
+      body: {
+        name: 'John',
+        cargo: 'chefia',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('idade'))
   })
 })
--- src/controllers/CreateEmployee/CreateEmployeeController.ts
+++ src/controllers/CreateEmployee/CreateEmployeeController.ts
@@ -1,8 +1,21 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
 export class CreateEmployeeController {
-  handle(httpRequest: any): any {
+  handle(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('name'),
+      }
+    }
+    if (!httpRequest.body.idade) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('idade'),
+      }
+    }
     return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
+      statusCode: 200,
     }
   }
 }
