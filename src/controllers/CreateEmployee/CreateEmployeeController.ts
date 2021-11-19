7220de4332a425fbcadfe012c0b4df81bc506de0
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-11-18 23:24:03 -0300

    feat: ensure CreateEmployeeController return 400 if property cargo not found

--- src/controllers/CreateEmployee/CreateEmployeeController.ts
+++ src/controllers/CreateEmployee/CreateEmployeeController.ts
@@ -1,19 +1,18 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+
 export class CreateEmployeeController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name'),
+    const requiredFields = ['nome', 'idade', 'cargo']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new MissingParamError(field),
+        }
       }
     }
-    if (!httpRequest.body.idade) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('idade'),
-      }
-    }
+
     return {
       statusCode: 200,
     }
