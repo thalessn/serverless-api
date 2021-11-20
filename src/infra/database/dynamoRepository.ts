57a276ad9e36ae778f3e603c3f6ded2540a4324b
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-11-20 14:09:11 -0300

    feat: add GetEmployeeRepository method to the DynamoRepository

--- src/infra/database/dynamoRepository.ts
+++ src/infra/database/dynamoRepository.ts
@@ -7,15 +7,21 @@ import {
   DeleteItemCommandInput,
   PutItemCommand,
   PutItemCommandInput,
+  GetItemCommandInput,
+  GetItemCommand,
 } from '@aws-sdk/client-dynamodb'
-import { marshall } from '@aws-sdk/util-dynamodb'
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb'
 import { v4 as uuidv4 } from 'uuid'
 import { DeleteEmployeeRepository } from '../../data/protocols/deleteEmployeeRepository'
+import { GetEmployeeRepository } from '../../data/protocols/getEEmployeeRepository'
 
 const TableName = 'employee'
 
 export class DynamoRepository
-  implements CreateEmployeeRepository, DeleteEmployeeRepository
+  implements
+    CreateEmployeeRepository,
+    DeleteEmployeeRepository,
+    GetEmployeeRepository
 {
   async add(employeeData: CreateEmployeeModel): Promise<EmployeeModel> {
     const employee = {
@@ -47,5 +53,28 @@ export class DynamoRepository
     const result = await dynamoClient.send(new DeleteItemCommand(params))
     if (result && result.$metadata.httpStatusCode !== 200)
       throw new Error('It was not possible to connect to the bank')
+  }
+
+  async get(id: string): Promise<EmployeeModel> {
+    const params: GetItemCommandInput = {
+      TableName,
+      Key: marshall({
+        id,
+      }),
+    }
+
+    const results = await dynamoClient.send(new GetItemCommand(params))
+    if (results && results.$metadata.httpStatusCode === 200) {
+      const emptyEmployee = {
+        id: '',
+        nome: '',
+        idade: '',
+        cargo: '',
+      }
+      const employee = unmarshall(results.Item || {}) as EmployeeModel
+      return employee
+    }
+
+    throw new Error('Method not implemented.')
   }
 }
