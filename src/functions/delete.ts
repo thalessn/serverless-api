23f834a4d0d016c330a7c5cdd3f120a5bc56eb97
Thales Nascimento <thales.silva.nascimento@gmail.com>
2021-11-20 13:01:55 -0300

    feat: add DeleteController on delete function

--- src/functions/delete.ts
+++ src/functions/delete.ts
@@ -3,20 +3,35 @@ import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
 } from 'aws-lambda'
+import { makeDeleteEmployeeController } from './factories/controllers/deleteEmployeeFactory'
 
 export const handler: APIGatewayProxyHandler = async (
-  _event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
-    const response = {
-      statusCode: 200,
-      body: 'HELLO YOU ARE MY FRIEND!!!',
+    const id = event.pathParameters?.id
+    if (id) {
+      const controller = makeDeleteEmployeeController()
+      const response = await controller.handle({
+        params: {
+          id,
+        },
+      })
+
+      return {
+        statusCode: response.statusCode,
+        body: JSON.stringify(response.body),
+      }
     }
-    return response
-  } catch (err) {
+
+    return {
+      statusCode: 400,
+      body: 'The param id was not found',
+    }
+  } catch (err: any) {
     return {
       statusCode: 500,
-      body: 'An error occured',
+      body: err.message,
     }
   }
 }
